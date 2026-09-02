3f1a03767bc4c90cca578fc32784c9f48b71171b
PathumHimantha <pathumh074@gmail.com>
2026-09-02 16:41:49 +0530

    Configure Vite base path for prod

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const App = () => (
       <AuthProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/prod">
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
